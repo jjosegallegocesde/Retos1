00b072b6a741ffef3f4cc29d6ebe52c9c5c09b4f
Juan <juan.gallegomesa@gmail.com>
2021-06-14 15:47:53 -0500

    Initial commit

---
+++ src/retos1/Retos1.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package retos1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan
+ */
+public class Retos1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        /* 1. Juan tiene N cantidad de pesos, Camila tiene la mitad de lo que posee Juan y Ricardo tiene 
+            la mitad de lo que poseen Camila y Juan Juntos.¿Puede JAVA ayudarme a calcular la 
+        cantidad de dinero de los 3?*/
+        
+        /*int dineroJuan,dineroCamila,dineroRicardo;
+        
+        Scanner entrada= new Scanner(System.in);
+        
+        System.out.println("Digita la cantidad de dinero de Juan: ");
+        dineroJuan=entrada.nextInt();
+        dineroCamila=(dineroJuan/2);
+        dineroRicardo=((dineroJuan+dineroCamila)/2);
+        
+        System.out.println("\n La cantidad de dinero de camila es: "+dineroCamila);
+        System.out.println("\n La cantidad de dinero de Ricardo es: "+dineroRicardo);*/
+        
+        
+        /* Una compañía de software contable, paga a su personal de ventas un salario de 3500000 mensuales, 
+        mas una comisión de 1500000 por cada licencia de software vendida menos el 5% de deducciones por impuestos. 
+        Codifica un programa que calcule e imprima el salario mensual de un vendedor dado*/
+        
+        /*Scanner entrada= new Scanner(System.in);
+        final int SALARIO_BASE=3500000;
+        final int VALOR_COMISION=1500000;
+        int ventasEmpleado;
+        
+        System.out.println("Digite el numero de ventas del empleado: ");
+        ventasEmpleado=entrada.nextInt();
+        
+        double valorComision=(ventasEmpleado*VALOR_COMISION)*0.5;
+        
+        double salarioTotal=SALARIO_BASE+((ventasEmpleado*VALOR_COMISION)-valorComision);
+        
+        System.out.println("Su salario este mes fue de: "+salarioTotal);*/
+        
+        
+        /* Hallar la edad de Pedro, sabiendo que dentro de 11 años tendrá la mitad del cuadrado 
+        de la edad que tenia hace 13 años*/
+        
+        /*Scanner entrada= new Scanner(System.in);
+        System.out.println("Resolvere una ecuación AX^2+BX+C=0");
+        
+        System.out.println("Digite el valor del coeficiente A");
+        int A=entrada.nextInt();
+
+        System.out.println("Digite el valor del coeficiente B");
+        int B=entrada.nextInt();
+        
+        System.out.println("Digite el valor del coeficiente C");
+        int C=entrada.nextInt();
+        
+        double x1= ((B*-1)+(Math.sqrt((Math.pow(B,2))-(4*A*C))))/(2*A);
+        double x2= ((B*-1)-(Math.sqrt((Math.pow(B,2))-(4*A*C))))/(2*A);*/
+        
+        
+        
+        /* En una finca entre gallinas y conejos, se cuentan 50 cabezas y 140 patas 
+        ¿Cuántas gallinas y conejos hay en la granja?*/
+        /*Scanner entrada= new Scanner(System.in);
+        
+        System.out.println("Digite coeficiente A");
+        int A= entrada.nextInt();
+        
+        System.out.println("Digite coeficiente B");
+        int B= entrada.nextInt();
+        
+        System.out.println("Digite coeficiente C");
+        int C= entrada.nextInt();
+        
+        System.out.println("Digite coeficiente D");
+        int D= entrada.nextInt();
+        
+        System.out.println("Digite coeficiente E");
+        int E= entrada.nextInt();
+        
+        System.out.println("Digite coeficiente F");
+        int F= entrada.nextInt();
+        
+        double x=((C*E)-(B*F))/((A*E)-(B*D));
+        double Y=((A*F)-(C*D))/((A*E)-(B*D));*/
+        
+        
+        /* Un terreno cuadrado tiene un área de N m2 ¿Cuántos metros de alambre 
+        se necesitan para cercarlo?*/
+        
+        /*Scanner entrada= new Scanner(System.in);
+        
+        System.out.println("Digite el area: ");
+        float area= entrada.nextFloat();
+        
+        float perimetro= (float) (Math.sqrt(area)*4);
+        
+        System.out.println("El perimetro es de: "+perimetro);*/
+        
+        
+    }
+    
+}
